09fde12f856badf57205d47ad7f337ef4a78a33d
Guo, Quan <guoquanscu@gmail.com>
2019-09-18 05:03:09 +0000

    Update postpone inference to show training.

--- examples/emr/config.py
+++ examples/emr/config.py
@@ -36,7 +36,7 @@ config = {
             'balance_factor': 1.5,
             'label_smoothing': 0.01,
             'focal_gamma': 2,
-            'inference_interval': 1,
+            'inference_interval': 5,
             'inference_training_set': False
         }
     },
